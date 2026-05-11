bf7322ddc247c2c819702e947a2cbc238121af7c
황의빈 <sjwj2o2838euejd@gmail.com>
2026-05-11 16:46:01 +0900

    ㅇㅇㅇ이마

---
+++ test.java
@@ -0,0 +1,22 @@
+
+public class test
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class test
+     */
+    public test()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
